a7d6d9346df0536b6495f7a24754698d92e65fcd
Ignatenko Olga <olga.ignatenko@soft-loft.eu>
2023-08-11 09:09:30 +0300

    Add project files.

---
+++ Smartwyre.DeveloperTest.Runner/RebateServiceTests.cs
@@ -0,0 +1,102 @@
+﻿
+using Moq;
+using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Types;
+using System;
+using Xunit;
+
+namespace Smartwyre.DeveloperTest.Runner
+{
+    public class RebateServiceTests
+    {
+        [Theory]
+        [InlineData(IncentiveType.FixedCashAmount, 1, SupportedIncentiveType.FixedCashAmount, 0, 0, 0, 0, false)]
+        [InlineData(IncentiveType.FixedCashAmount, 0, SupportedIncentiveType.FixedCashAmount, 0, 0, 0, 0, false)]
+        [InlineData(IncentiveType.FixedCashAmount, 1, SupportedIncentiveType.FixedRateRebate, 0, 0, 0, 1, true)]
+        [InlineData(IncentiveType.FixedCashAmount, 0, SupportedIncentiveType.FixedRateRebate, 0, 0, 0, 0, false)]
+        [InlineData(IncentiveType.FixedRateRebate, 0, SupportedIncentiveType.FixedRateRebate, 0, 0, 0, 0, false)]
+        [InlineData(IncentiveType.FixedRateRebate, 0, SupportedIncentiveType.FixedRateRebate, 1, 0, 0, 0, false)]
+        [InlineData(IncentiveType.FixedRateRebate, 0, SupportedIncentiveType.FixedRateRebate, 1, 1, 0, 0, false)]
+        [InlineData(IncentiveType.FixedRateRebate, 0, SupportedIncentiveType.FixedCashAmount, 1, 1, 2, 2, true)]
+        [InlineData(IncentiveType.AmountPerUom, 1, SupportedIncentiveType.FixedCashAmount, 1, 1, 2, 2, true)]
+        [InlineData(IncentiveType.AmountPerUom, 1, SupportedIncentiveType.AmountPerUom, 1, 1, 2, 0, false)]
+        [InlineData(IncentiveType.AmountPerUom, 0, SupportedIncentiveType.FixedCashAmount, 1, 1, 2, 0, false)]
+        [InlineData(IncentiveType.AmountPerUom, 1, SupportedIncentiveType.FixedCashAmount, 1, 1, 0, 0, false)]
+        public void SetRebateIncentiveWhenProductNotNull(IncentiveType incentiveType,
+            decimal amount,
+            SupportedIncentiveType supportedIncentivesType,
+            decimal percentage,
+            decimal price,
+            decimal volume,
+            decimal rebateAmount,
+            bool success
+            )
+        {
+            var rebateDataStore = new Mock<IRebateDataStore>();
+            var productDataStore = new Mock<IProductDataStore>();
+            var rebateService = new RebateService(rebateDataStore.Object, productDataStore.Object);
+            var result = new CalculateRebateResult();
+
+            var request = new CalculateRebateRequest()
+            {
+                Volume = volume
+            };
+
+            var product = new Product()
+            {
+                Price = price,
+                SupportedIncentives = supportedIncentivesType
+            };
+
+            var rebate = new Rebate()
+            {
+                Amount = amount,
+                Incentive = incentiveType,
+                Percentage = percentage,
+            };
+
+            var res = rebateService.SetRebateIncentive(rebate, product, ref result, request);
+
+            Assert.True(result.Success == success);
+            Assert.True(res  == rebateAmount);
+        }
+
+        [Theory]
+        [InlineData(IncentiveType.FixedRateRebate)]
+        [InlineData(IncentiveType.AmountPerUom)]
+        public void SetRebateIncentiveWhenProductNull(IncentiveType incentiveType)
+        {
+            var rebateDataStore = new Mock<IRebateDataStore>();
+            var productDataStore = new Mock<IProductDataStore>();
+            var rebateService = new RebateService(rebateDataStore.Object, productDataStore.Object);
+            var result = new CalculateRebateResult();
+
+            var rebate = new Rebate()
+            {
+                Amount = 0,
+                Incentive = incentiveType,
+                Percentage = 0,
+            };
+
+            var res = rebateService.SetRebateIncentive(rebate, null, ref result, null);
+
+            Assert.True(result.Success == false);
+            Assert.True(res == 0);
+        }
+
+        [Fact]
+        public void SetRebateIncentiveWhenRebateNull()
+        {
+            var rebateDataStore = new Mock<IRebateDataStore>();
+            var productDataStore = new Mock<IProductDataStore>();
+            var rebateService = new RebateService(rebateDataStore.Object, productDataStore.Object);
+            var result = new CalculateRebateResult();
+
+            var res = rebateService.SetRebateIncentive(null, null, ref result, null);
+
+            Assert.True(result.Success == false);
+            Assert.True(res == 0);
+        }
+    }
+}
---
+++ Smartwyre.DeveloperTest/Services/RebateService.cs
@@ -0,0 +1,131 @@
+﻿using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Types;
+
+namespace Smartwyre.DeveloperTest.Services;
+
+public class RebateService : IRebateService
+{
+    private readonly IRebateDataStore _rebateDataStore;
+    private readonly IProductDataStore _productDataStore;
+
+    public RebateService(IRebateDataStore rebateDataStore, IProductDataStore productDataStore) {
+        _rebateDataStore = rebateDataStore;
+        _productDataStore = productDataStore;
+    }
+    public CalculateRebateResult Calculate(CalculateRebateRequest request)
+    {
+        Rebate rebate = _rebateDataStore.GetRebate(request.RebateIdentifier);
+        Product product = _productDataStore.GetProduct(request.ProductIdentifier);
+
+        var result = new CalculateRebateResult();
+
+        var rebateAmount = SetRebateIncentive(rebate, product, ref result, request);
+
+        if (result.Success)
+        {
+            var storeRebateDataStore = new RebateDataStore();
+            storeRebateDataStore.StoreCalculationResult(rebate, rebateAmount);
+        }
+
+        return result;
+    }
+
+    private bool IsProductSupportedIncentives(Product product, SupportedIncentiveType supportedIncentivesType) =>
+        !product.SupportedIncentives.HasFlag(supportedIncentivesType);
+
+    public decimal SetRebateIncentive(Rebate rebate, Product product, ref CalculateRebateResult result, CalculateRebateRequest request) {
+
+        var rebateAmount = 0m;
+        if (rebate == null)
+        {
+            result.Success = false;
+            return rebateAmount;
+        }
+
+        switch (rebate.Incentive)
+        {
+            case IncentiveType.FixedCashAmount:
+                rebateAmount = SetRebateIncentiveFixedCashAmount(rebate, product, ref result);
+                break;
+
+            case IncentiveType.FixedRateRebate:
+                rebateAmount = SetRebateIncentiveFixedRateRebate(rebate, product, ref result, request);
+                break;
+
+            case IncentiveType.AmountPerUom:
+
+                rebateAmount = SetRebateIncentiveAmountPerUom(rebate, product, ref result, request);
+                break;
+        }
+        return rebateAmount;
+
+    }
+    private decimal SetRebateIncentiveFixedCashAmount(Rebate rebate, Product product, ref CalculateRebateResult result) {
+        var rebateAmount = 0m;
+        if (!IsProductSupportedIncentives(product, SupportedIncentiveType.FixedCashAmount))
+        {
+            result.Success = false;
+        }
+        else if (rebate.Amount == 0)
+        {
+            result.Success = false;
+        }
+        else
+        {
+            result.Success = true;
+            rebateAmount = rebate.Amount;
+        }
+        return rebateAmount;
+    }
+
+    private decimal SetRebateIncentiveFixedRateRebate(Rebate rebate, 
+        Product product,
+        ref CalculateRebateResult result,
+        CalculateRebateRequest request)
+    {
+        var rebateAmount = 0m;
+        if (product == null)
+        {
+            result.Success = false;
+        }
+        else if (!IsProductSupportedIncentives(product, SupportedIncentiveType.FixedRateRebate))
+        {
+            result.Success = false;
+        }
+        else if (rebate.Percentage == 0 || product.Price == 0 || request.Volume == 0)
+        {
+            result.Success = false;
+        }
+        else
+        {
+            rebateAmount += product.Price * rebate.Percentage * request.Volume;
+            result.Success = true;
+        }
+        return rebateAmount;
+    }
+
+    private decimal SetRebateIncentiveAmountPerUom(Rebate rebate,
+       Product product,
+       ref CalculateRebateResult result,
+       CalculateRebateRequest request)
+    {
+        var rebateAmount = 0m;
+        if (product == null)
+        {
+            result.Success = false;
+        } else if (!IsProductSupportedIncentives(product, SupportedIncentiveType.AmountPerUom))
+        {
+            result.Success = false;
+        }
+        else if (rebate.Amount == 0 || request.Volume == 0)
+        {
+            result.Success = false;
+        }
+        else
+        {
+            rebateAmount += rebate.Amount * request.Volume;
+            result.Success = true;
+        }
+        return rebateAmount;
+    }
+}
